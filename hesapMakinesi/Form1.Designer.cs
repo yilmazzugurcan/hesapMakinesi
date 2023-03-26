53edca42732c21c5d95114fdefaea73e8b03250b
Uğurcan Yılmaz <yilmazzugurcan@gmail.com>
2023-03-26 19:47:53 +0300

    Add project files.

---
+++ hesapMakinesi/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace hesapMakinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.cmbOperation = new System.Windows.Forms.ComboBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(412, 48);
+            this.txtNumber1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(181, 22);
+            this.txtNumber1.TabIndex = 0;
+            this.txtNumber1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtNumber1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(412, 93);
+            this.txtNumber2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(181, 22);
+            this.txtNumber2.TabIndex = 1;
+            this.txtNumber2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cmbOperation
+            // 
+            this.cmbOperation.FormattingEnabled = true;
+            this.cmbOperation.Items.AddRange(new object[] {
+            "Topla",
+            "Çıkar",
+            "Çarp",
+            "Böl"});
+            this.cmbOperation.Location = new System.Drawing.Point(412, 197);
+            this.cmbOperation.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbOperation.Name = "cmbOperation";
+            this.cmbOperation.Size = new System.Drawing.Size(180, 24);
+            this.cmbOperation.TabIndex = 2;
+            this.cmbOperation.SelectedIndexChanged += new System.EventHandler(this.cmbOperation_SelectedIndexChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(412, 291);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(183, 70);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Hesapla";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(405, 408);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.cmbOperation);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.ComboBox cmbOperation;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ hesapMakinesi/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbOperation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            /*  double number1 = double.Parse(txtNumber1.Text);
+              double number2 = double.Parse(txtNumber2.Text);
+              string operation = cmbOperation.SelectedItem.ToString();
+              double result = 0;*/
+
+            if (string.IsNullOrEmpty(txtNumber1.Text) && string.IsNullOrEmpty(txtNumber2.Text))
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurun.");
+                return;
+            }
+
+
+            double number1 = double.Parse(txtNumber1.Text);
+            double number2 = double.Parse(txtNumber2.Text);
+            string operation = cmbOperation.SelectedItem.ToString();
+            double result = 0;
+
+            switch (operation)
+            {
+                case "Topla":
+                    result = number1 + number2;
+                    break;
+                case "Çıkar":
+                    result = number1 - number2;
+                    break;
+                case "Çarp":
+                    result = number1 * number2;
+                    break;
+                case "Böl":
+                    if (cmbOperation.SelectedItem.ToString() == "Böl" && txtNumber2.Text == "0")
+                    {
+                        label1.Text = "Sıfıra Bölme Hatası";
+                        return;
+                    }
+                    result = number1 / number2;
+                    break;
+                default:
+                    break;
+            }
+           
+            label1.Text = result.ToString();
+        }
+    }
+}
